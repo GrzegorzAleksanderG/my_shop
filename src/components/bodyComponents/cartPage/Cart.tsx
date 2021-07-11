df9c46ab1114d85b527def0921e19d29578c9739
Gacek <gzgk@gft.com>
2021-07-11 17:32:01 +0200

    count but with bugs temporarily

--- src/components/bodyComponents/cartPage/Cart.tsx
+++ src/components/bodyComponents/cartPage/Cart.tsx
@@ -10,7 +10,8 @@ const Cart = () => {
         { field: 'name', headerName: 'Name', width: 150 },
         { field: 'category', headerName: 'Category', width: 140 },
         { field: 'description', headerName: 'Description', width: 150 },
-        { field: 'price', headerName: 'Price', width: 110 }
+        { field: 'price', headerName: 'Price', width: 110 },
+        { field: 'count', headerName: 'Count', width: 120 }
     ];
 
     return (
--- src/reducers/cartReducer.tsx
+++ src/reducers/cartReducer.tsx
@@ -1,12 +1,32 @@
 import { ACTION_TYPES } from "../actions/actionTypes"
 
 export const cartReducer = (state: any[] = [], action: any) => {
+    console.log(action);
     switch (action.type) {
         case ACTION_TYPES.ADD_TO_CART_ASYNC:
-            state = state.concat([action.payload]);
+            if (!state.some((x) => { return x.id === action.payload.id })) {
+                action.payload.count = 1;
+                state = state.concat(action.payload);
+            } else {
+                state = state.map((x) => {
+                    if (x.id === action.payload.id) {
+                        x.count++;
+                    }
+                    return x;
+                });
+            }
             return state
         case ACTION_TYPES.REMOVE_FROM_CART_ASYNC:
-            state = state.filter((x) => {return x.id !== action.payload.id});
+            if (state.some((x) => { return x.id === action.payload.id && x.count > 1 })) {
+                state = state.map((x) => {
+                    if (x.id === action.payload.id) {
+                        x.count--;
+                    }
+                    return x;
+                });
+            } else {
+                state = state.filter((x) => { return x.id !== action.payload.id });
+            }
             return state
         default:
             return state;
