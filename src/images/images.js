0f297a3f962fce778c2543bc681e5ec8506ccd06
Gacek <gzgk@gft.com>
2021-09-18 19:34:07 +0200

    carousel added

---
+++ src/images/images.js
@@ -0,0 +1,13 @@
+const images = [
+    { id: 1, src: 'Acer_SB220Q.jpg', title: 'Acer SB220Q' },
+    { id: 2, src: 'AMD_Ryzen_7_3700x.jpg', title: 'AMD Ryzen 7 3700x' },
+    { id: 3, src: 'ASUS_ROG_Strix.jpg', title: 'ASUS ROG Strix' },
+    { id: 4, src: 'Gigabyte_Z590.jpg', title: 'Gigabyte Z590' },
+    { id: 5, src: 'Intel_Core_i9.jpg', title: 'Intel Core i9' },
+    { id: 6, src: 'Lenovo_G34w-10.jpg', title: 'Lenovo G34w-10' },
+    { id: 7, src: 'Logitech_MK270.jpg', title: 'Logitech MK270' },
+    { id: 8, src: 'MSI_Gaming_GeForce.jpg', title: 'MSI Gaming GeForce' },
+    { id: 9, src: 'Redragon_S101.jpg', title: 'Redragon S101' },
+    { id: 10, src: 'ZOTAC_Gaming_GeForce.jpg', title: 'ZOTAC Gaming GeForce' }
+];
+export default images;
