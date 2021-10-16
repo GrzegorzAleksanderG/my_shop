9dde33015bbafe8ba26a3cba075e60b7a0bcdac2
Gacek <gzgk@gft.com>
2021-10-17 00:48:38 +0200

    fb login init

--- src/components/bodyComponents/loginPage/LoginScreen.tsx
+++ src/components/bodyComponents/loginPage/LoginScreen.tsx
@@ -4,21 +4,26 @@ import "./LoginScreen.css";
 import { logUserAction } from '../../../actions/loginUserActions';
 import { useState } from 'react';
 import { RegisterReducerType, StateType } from '../../../reducers/stateReducerTypes';
+import FacebookLogin, { ReactFacebookLoginInfo } from 'react-facebook-login';
 
 const LoginScreen = () => {
     const dispatch = useDispatch();
     const [mail, setMail] = useState("");
     const [pass, setPass] = useState("");
-    const registeredUsersSelector = useSelector((state : StateType) => state.registerReducer)
+    const registeredUsersSelector = useSelector((state: StateType) => state.registerReducer)
 
     const handleOnClick = () => {
-        if(Object.values(registeredUsersSelector).some((x : RegisterReducerType) => x.mail === mail && x.pass === pass)){
-            dispatch(logUserAction({mail, pass}));
-        }else{
+        if (Object.values(registeredUsersSelector).some((x: RegisterReducerType) => x.mail === mail && x.pass === pass)) {
+            dispatch(logUserAction({ mail, pass }));
+        } else {
             alert("User or password is not valid. Access denied.")
         }
         setMail("");
         setPass("");
+    }
+
+    const responseFacebook = (response: ReactFacebookLoginInfo) => {
+        console.log(response);
     }
 
     return (
@@ -27,27 +32,35 @@ const LoginScreen = () => {
                 <FormGroup>
                     <FormControl>
                         <InputLabel htmlFor="my-email">Email address</InputLabel>
-                        <Input id="my-email" aria-describedby="my-email-text" type="email" onChange={(e) => setMail(e.target.value)} value={mail}/>
+                        <Input id="my-email" aria-describedby="my-email-text" type="email" onChange={(e) => setMail(e.target.value)} value={mail} />
                         <FormHelperText id="my-email-text">Type your email</FormHelperText>
                     </FormControl>
                     <FormControl>
                         <InputLabel htmlFor="my-password">Password</InputLabel>
-                        <Input id="my-password" aria-describedby="my-password-text" type="password" onChange={(e) => setPass(e.target.value)}  value={pass}/>
+                        <Input id="my-password" aria-describedby="my-password-text" type="password" onChange={(e) => setPass(e.target.value)} value={pass} />
                         <FormHelperText id="my-password-text">Type your password</FormHelperText>
                     </FormControl>
-                    <Button variant="contained" type="button" onClick={()=>{handleOnClick()}} className="submit-registration">Login</Button>
+                    <Button variant="contained" type="button" onClick={() => { handleOnClick() }} className="submit-registration">Login</Button>
+                    <FacebookLogin
+                        appId="562118384400275"
+                        autoLoad={true}
+                        fields="name,email,picture"
+                        scope="public_profile,user_friends"
+                        callback={responseFacebook}
+                        icon="fa-facebook"
+                    />
                 </FormGroup>
             </form>
         </div>
     )
 }
 
-const mapStateToProps = (state:StateType) => {
+const mapStateToProps = (state: StateType) => {
     return {
         loginReducer: state.loginReducer
     }
 }
 
-const mapDispatchToProps = {logUserAction}
+const mapDispatchToProps = { logUserAction }
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginScreen);
